364ce5c1c2f25553e44bcb0e6bc5346bdf3a26bb
shivam <shivamkumaar01@gmail.com>
2025-05-19 10:43:49 +0530

    dynamic route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Param, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController{
 @Get()
-getUsers(){
+getUsers(@Query() query:any){
     const UserService=new UsersService();
+    console.log(query);
     return UserService.getAllUsers();
 }
-@Get(':id/:name/:gender?')
-getUserById(){
+@Get(':id/')
+getUserById(@Param('id') id:any){
+    // console.log(param)
+    const UserService=new UsersService();
+    return UserService.getUserById(+id)
 
 }
 
