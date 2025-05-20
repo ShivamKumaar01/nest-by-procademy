e5a9451c28aa600e4390947cd349bfe9d0abc22a
ShivamKumaar01 <ShivamKumaar01@gmail.com>
2025-05-20 07:37:16 +0530

    dependency injection is done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,48 +1,86 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from "@nestjs/common";
-import { UsersService } from "./users.service";
-// import { CreateUserDto } from "./dtos/create-user.dto";
+// import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from "@nestjs/common";
+// import { UsersService } from "./users.service";
+// // import { CreateUserDto } from "./dtos/create-user.dto";
+
+// @Controller('users')
+// export class UsersController {
+//     constructor(
+//         usersService : UsersService
+//     ) {}
+//     @Get()
+//     getUsers(@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+//         @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number) {
+//         const UserService = new UsersService();
+//         console.log(limit, page)
+
+//         return UserService.getAllUsers();
+
+
+//     }
+//     // getUsers(@Query() query:any){
+//     //     const UserService=new UsersService();
+//     //     // console.log(query);
+//     //     if(query.gender){
+//     //         return UserService.getAllUsers().filter(u=>u.gender===query.gender)
+//     //     }
+//     //     else{
+//     //         return UserService.getAllUsers();
+//     //     }
+
+//     // }
+//     @Get(':id/')
+//     getUserById(@Param('id', ParseIntPipe) id: any) {
+//         // console.log(param)
+//         const UserService = new UsersService();
+//         // console.log(typeof(id))
+//         return UserService.getUserById(id)
+
+//     }
+
+
+//     @Post()
+//     createUser(@Body(new ValidationPipe()) user: any) {
+//         // const user={id:3,name:'marry',email:'marry@gmail.com',age:26,gender:'female',isMarried:true}
+//         // const UserService=new UsersService();
+//         // this.usersService.createUser(user);
+//         return 'a new user is created successfully'
+//     }
+// }
+
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(
-        usersService : UsersService
-    ) {}
-    @Get()
-    getUsers(@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
-        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number) {
-        const UserService = new UsersService();
-        console.log(limit, page)
+  constructor(private readonly usersService: UsersService) {} 
 
-        return UserService.getAllUsers();
+  @Get()
+  getUsers(
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit, page);
+    return this.usersService.getAllUsers(); 
+  }
 
+  @Get(':id/')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserById(id); 
+  }
 
-    }
-    // getUsers(@Query() query:any){
-    //     const UserService=new UsersService();
-    //     // console.log(query);
-    //     if(query.gender){
-    //         return UserService.getAllUsers().filter(u=>u.gender===query.gender)
-    //     }
-    //     else{
-    //         return UserService.getAllUsers();
-    //     }
-
-    // }
-    @Get(':id/')
-    getUserById(@Param('id', ParseIntPipe) id: any) {
-        // console.log(param)
-        const UserService = new UsersService();
-        // console.log(typeof(id))
-        return UserService.getUserById(id)
-
-    }
-
-
-    @Post()
-    createUser(@Body(new ValidationPipe()) user: any) {
-        // const user={id:3,name:'marry',email:'marry@gmail.com',age:26,gender:'female',isMarried:true}
-        // const UserService=new UsersService();
-        // this.usersService.createUser(user);
-        return 'a new user is created successfully'
-    }
+  @Post()
+  createUser(@Body(new ValidationPipe()) user: any) {
+    this.usersService.createUser(user); 
+    return 'A new user is created successfully';
+  }
 }
